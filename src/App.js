6c83621d53f75b2a1158d62c5a5e4944d14e7668
seik <seik1224@naver.com>
2024-11-16 13:45:39 +0900

    .

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function App() {
     setNewTask(e.target.value)
   }
 
+  const handleAddTask = () => {
+    setTasks([...tasks, newTask]);
+    setNewTask("");
+  }
+
   return (
     <>
       <div className="min-h-screen bg-gray-100 flex items-center justify-center">
@@ -24,14 +29,16 @@ function App() {
               onChange={handleInputChange}
               className="flex-grow p-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-r-md hover:bg-blue-600">
+            <button 
+            onClick={handleAddTask}
+            className="bg-blue-500 text-white px-4 py-2 rounded-r-md hover:bg-blue-600">
               추가
             </button>
           </div>
           <ul>
             {/* li는 따로 TodoItem이라는 자식 컴포넌트를 만들어서 관리해주세요. */}
             <li className="bg-white shadow-md rounded-lg px-4 py-2 mb-2 flex justify-between items-center cursor-pointer hover:bg-gray-100">
-              <span>여기에 입력한 값을 넣어주세요</span>
+              <span>{tasks}</span>
               <button className="text-red-500 hover:text-red-700">삭제</button>
             </li>
           </ul>
